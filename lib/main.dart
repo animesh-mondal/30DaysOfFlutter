8ece46abc4555c20bcf30258e81e5893d17c6c17
Animesh <animesh.gopalpur@gmail.com>
2021-05-16 06:39:50 +0530

    Day 2 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 main() {
   runApp(MyApp());
@@ -8,15 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Catalog'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text('Welcome to my youtube channel..'),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
